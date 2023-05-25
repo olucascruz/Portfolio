d382c558efdb8d2c657d5d0b660a6ea04a51766b
LucasCruz <lca.snf21@uea.edu.br>
2023-05-25 08:27:42 -0400

    feat: add links on header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 function Header(){
     return(
         <header className="App-header">
-            <Link to={"/"}><h1>Lucas Cruz</h1></Link>
+            <Link to={"/"} className="Name"><h1>Lucas Cruz</h1></Link>
+
+            <nav className="Categories">
+                <Link to={"/"} className="CategoryProject"><p>Web</p></Link>
+                <Link to={"/"} className="CategoryProject"><p>Mobile</p></Link>
+                <Link to={"/"} className="CategoryProject"><p>Game</p></Link>
+            </nav>
         </header>
         )
 }
