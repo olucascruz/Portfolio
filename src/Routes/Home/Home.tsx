b636044d421cc899fdda091a65af2ac022c513af
LucasCruz <lca.snf21@uea.edu.br>
2023-05-25 08:26:44 -0400

    feat: update home

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -1,13 +1,38 @@
 import './Home.css'
-
+import DropDownMenu from '../../Components/DropDownMenu/DropDownMenu'
+import CardProject from '../../Components/CardProject/CardProject'
 function Home(){
     return(
         <div className="App-home">
-            <div className="Home-description">
-                <h4>Ola sou um desenvolvedor de software.</h4>    
-                <button>Projetos</button>
-            </div>
-        </div> 
+            <section className="FirstSection">
+                <div className="Home-description">
+                    <p>Ola sou um desenvolvedor de software.</p>    
+                
+                </div>
+            </section>
+            <section className="AboutMe">
+                <h2>Sobre mim</h2>
+                <p>hey bro sou um dev web, mobile, unity</p>
+            </section>
+
+            <section className="SectionWeb SectionCategory">
+                <h3>Projetos Web</h3>
+                <CardProject 
+                titleCard={"Casa dos Jogos"} 
+                tech = {["aaa"]}
+                description={"Um site para upar jogos de desenvolvedores indie"}
+                />
+            </section>
+            
+            <section className="SectionMobile SectionCategory">
+
+            </section>
+            
+            <section className="SectionGame SectionCategory">
+
+            </section>
+        </div>
+         
     )
 }
 
