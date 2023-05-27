8f90d49746ffd71ca01e7a7d803b5a8aa09c7dc3
LucasCruz <lca.snf21@uea.edu.br>
2023-05-27 14:06:49 -0400

    feat: create CardTechnologies

---
+++ src/Components/CardTechnologies/CardTechnologies.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardActions, Button } from "@mui/material"
+
+
+function CardTechnologies(){
+    return (
+            <Card className= "CardTechnologies" sx={{ minWidth: 20 }}>
+                <CardContent className="CardContent">
+                    
+
+                    
+                </CardContent>
+            </Card>
+        )
+}
+
+
+export default CardTechnologies
