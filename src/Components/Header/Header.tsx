161e0252d97397464940448d2327c91dc72818aa
LucasCruz <lca.snf21@uea.edu.br>
2023-06-02 15:22:03 -0400

    feat: add icon button switch theme

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
 import MyContext from '../../Context';
-
+import {BsMoon, BsSun} from 'react-icons/bs'
 
 function Header(){
+
+    const [ isDark, setIsDark ] = useState(true)
     const {switchThemeHandler, theme} = useContext(MyContext)
+    const onClickSwitchTheme = () =>{
+        switchThemeHandler()
+        setIsDark(!isDark)
+    }
     return(
         <header className={`App-header ${theme}`}>
             <Link to={"/"} className="Name"><h1>Lucas Cruz</h1></Link>
@@ -15,7 +21,9 @@ function Header(){
                 <Link to={"/"} className="CategoryProject"><p>Mobile</p></Link>
                 <Link to={"/"} className="CategoryProject"><p>Game</p></Link>
             </nav>
-            <button onClick={switchThemeHandler}>Trocar cor</button>
+            <button className={`BtSwitchTheme ${theme}`} onClick={onClickSwitchTheme}>
+                {isDark ?<BsMoon size={50}></BsMoon>:<BsSun size={50}></BsSun>}
+            </button>
         </header>
         )
 }
