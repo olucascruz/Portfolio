87d2ec3d9d11735a665355867e8149f07390df48
LucasCruz <lca.snf21@uea.edu.br>
2023-12-23 12:05:27 -0400

    fix: refactor path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router'
 import MyContext from './Context';
 
 function App() {
